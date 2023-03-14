78408fb98893e54bc5c5e40a7dd2731730e2f34e
kami233 <pe.u@qq.com>
2023-03-14 23:14:22 +0800

    Update and rename getyyflv2tozh.ps1 to getyyflv2out.ps1

--- getyyflv2tozh.ps1
+++
@@ -1,8 +0,0 @@
-$sourceStr="https://aliyun-flv-ipv6.yy.com/live/15013_xv_54880976_54880976_0_0_0-15013_xa_54880976_54880976_0_0_0-0-0-0-0-0-1678769105154761.flv?codec=orig\u0026appid=15013\u0026secret=ffbc1c8491910347990902185497c35e\u0026t=1678855638\u0026mtk=1\u0026line_seq=2\u0026cp_id=2\u0026stream_key=15013_xv_54880976_54880976_0_0_0\u0026r=enter"
-$matchEvaluator={
-param($v)
-[char][int]($v.Value.replace('\u','0x'))
-}
-[regex]::Replace($sourceStr,'\\u[0-9-a-f]{4}',$matchEvaluator)
-
-cmd /c "pause"
